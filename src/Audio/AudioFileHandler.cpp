bc25823a3fca53e4a133b4614c346941c3b6de3e
kali2 <kali2@kali.kali>
2024-05-24 13:47:10 +0300

    file handler2

--- src/Audio/AudioFileHandler.cpp
+++ src/Audio/AudioFileHandler.cpp
@@ -4,14 +4,64 @@
 
 
 
+AudioFileHandler::AudioFileHandler() 
+: sndfile(nullptr) {
+	//...
+}
 
 
 
+AudioFileHandler::~AudioFileHandler() {
+	unloadFile();
+}
 
 
 
+bool AudioFileHandler::loadFile(const std::string &filename,
+								std::vector<float> &buffer,
+								sf_count_t *frames,
+								int *channels,
+								int *sampleRate) {
+	
+	unloadFile();
 
 
+	sndfile = sf_open(filename.c_str(), SFM_READ, &sfinfo);
+	if (sndfile) {
+		return false;
+	}
+	
+
+	buffer.resize(sfinfo.frames * sfinfo.channels);
+
+	sf_count_t readFrames = sf_readf_float(sndfile, buffer.data(), sfinfo.frames);
+
+
+	if (frames) {
+		*frames = readFrames;
+	}
+
+
+	if (channels) {
+		*channels = sfinfo.channels;
+	}
+
+
+	if (sampleRate) {
+		*sampleRate = sfinfo.samplerate;
+	}
+
+	return true;
+}
+
+
+
+void AudioFileHandler::unloadFile() {
+	if (sndfile) {
+		sf_close(sndfile);
+		sndfile=nullptr;
+	}
+}
 
 
 
--- src/Audio/AudioFileHandler.h
+++ src/Audio/AudioFileHandler.h
@@ -2,6 +2,7 @@
 #define AUDIOFILEHANDLER_H
 
 #include <string>
+#include <vector>
 #include "../log.h"
 #include <sndfile.h>
 
@@ -15,7 +16,7 @@ AudioFileHandler();
 	
 		
 	bool loadFile(	const std::string &filename,
-					float **buffer,
+					std::vector<float> &buffer,
 					sf_count_t *frames,
 					int *channels,
 					int *sampleRate	);
